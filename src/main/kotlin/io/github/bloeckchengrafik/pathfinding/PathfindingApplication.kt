1d07b02aef02f15ad818d349147e0d492c7d7662
Bloeckchengrafik <37768199+Bloeckchengrafik@users.noreply.github.com>
2022-04-18 19:09:42 +0200

    Initial Commit

---
+++ src/main/kotlin/io/github/bloeckchengrafik/pathfinding/PathfindingApplication.kt
@@ -0,0 +1,11 @@
+package io.github.bloeckchengrafik.pathfinding
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PathfindingApplication
+
+fun main(args: Array<String>) {
+    runApplication<PathfindingApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/bloeckchengrafik/pathfinding/PathfindingApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.bloeckchengrafik.pathfinding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PathfindingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
